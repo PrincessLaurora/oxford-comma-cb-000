a5fbc6271f131a1bdb720504c3ffc655839f3312
Laura Felce <crepu83@yahoo.it>
2018-11-15 12:01:34 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,9 @@
 def oxford_comma(array)
 if array.lenght = 1
 array.join (" , ")
+elsif array.lenght = 2
+array.join (" and ") 
 else
-array.join (" , ") << "and"
+array.join (" , ") << array.join (" and ") 
 end
 end
