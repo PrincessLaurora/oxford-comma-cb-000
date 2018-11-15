fcc856c17c15832db5e83b4ad77d1571489dbed6
Laura Felce <crepu83@yahoo.it>
2018-11-15 14:27:28 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ if array.length == 1
 array.join
 elsif array.length == 2
 array.join(" and ")
-elsif array.length >= 3
+elsif array.length > 2
 array.join(", ") && array[-1].insert(0, " and ")
 end
 end
